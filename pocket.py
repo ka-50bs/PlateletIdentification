687d0da0c64c024eb9d6b6d37e5c57dd09051700
ka-50bs <59329750+ka-50bs@users.noreply.github.com>
2024-06-04 11:07:07 +0300

    Add files via upload

---
+++ pocket.py
@@ -0,0 +1,44 @@
+class Purifier():
+    def zeroDeletion(self):
+        pass
+    def multiParticleRegistration(self):
+        pass
+
+class Drawer():
+    def drawMap(self):
+        pass
+
+    def drawGating(self):
+        pass
+    
+    def drawIdentification(self):
+        pass
+		
+class PltIdentifier():
+    def identifyByMetric(self):
+        pass
+
+    def identifyByMap(self):
+        pass
+
+    def __metric(self):
+        pass
+
+class AutoGater():
+    def makeMap(self):
+        pass
+    
+    def makeAutoGating(self):
+        pass
+
+    def __cmMap(self):
+        pass
+
+    def __integralMap(self):
+        pass
+
+    def __multifactorMap(self):
+        pass
+
+class InverseSolver():
+    def 
